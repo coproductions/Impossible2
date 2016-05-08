6879b1ce8fb46c15b67ecaae6d103f042b83ef08
coproductions <constantin.dumba@gmail.com>
2016-05-08 15:58:18 -0700

    adds progress bar page 27

--- client/modules/items/components/EditItem.jsx
+++ client/modules/items/components/EditItem.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Col, Panel, Input, ButtonInput } from 'react-bootstrap';
+import { Col, Panel, Input, ButtonInput , Glyphicon} from 'react-bootstrap';
+import DateTimeField from 'react-bootstrap-datetimepicker';
 
 const EditItem = ({content}) => (
   <Col xs={12} sm={6} smOffset={3}>
      <Panel>
+       <a href="/"><Glyphicon glyph="chevron-left"></Glyphicon> Back to Items</a>
        <h1>Edit Item</h1>
        <form>
          <Input type="text" placeholder="Name" />
          <Input type="textarea" placeholder="Description" />
+          <DateTimeField />
          <ButtonInput bsStyle="primary" type="submit" value="Save Item"/>
         </form>
       </Panel>
