7de92fe69d149bfcfcda9e8d44822030b214b22a
coproductions <constantin.dumba@gmail.com>
2016-05-08 14:25:20 -0700

    up to edit item form

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import {mount} from 'react-mounter';
 import Layout from './components/MainLayout.jsx';
 import ItemList from '../items/components/ItemList.jsx';
+import EditItem from '../items/components/EditItem.jsx';
 
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(Layout);
+
   FlowRouter.route('/', {
     name: 'items.list',
     action() {
@@ -13,4 +15,14 @@ export default function (injectDeps, {FlowRouter}) {
       });
     }
   });
+
+  FlowRouter.route('/edit',{
+    name: 'items.edit',
+    action(){
+      mount(MainLayoutCtx,{
+        content: ()=>(<EditItem/>)
+      });
+    }
+  });
+
 }
--- client/modules/items/components/ItemList.jsx
+++ client/modules/items/components/ItemList.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Item from './Item.jsx';
 
-const ItemList = ({content = () => null}) => (
-  <div>
-    <p>This is where the items will be.</p>
-  </div>
+const ItemList = ({content}) => (
+  <Row className="show-grid">
+    <Col xs={12}>
+       <Item/>
+     </Col>
+  </Row>
 );
+
 export default ItemList;
