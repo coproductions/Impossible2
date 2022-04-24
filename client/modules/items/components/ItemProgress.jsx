05a6227417b0f30a710b7c85aff9325bce2e907b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-24 21:48:22 +0800

    Delete client directory

--- client/modules/items/components/ItemProgress.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ProgressBar } from 'react-bootstrap';
-
-const ItemProgress = ({content}) => (
-  <ProgressBar now={70} label="%(percent)s%"></ProgressBar>
-);
-
-export default ItemProgress;
